e8d16c38535132fa0e4022390cf16b0a58caed70
manu-hn <manuu.hn11@gmail.com>
2024-06-25 15:05:00 +0530

    "Added new route for AfterSubmission component, updated HeaderSection component with new styles and layout, modified InputField component with new text sizes, updated useRenderInput hook with new label styles, and updated useSubmitFormData hook with navigation to /submitted route"

--- src/utils/hooks/useSubmitFormData.jsx
+++ src/utils/hooks/useSubmitFormData.jsx
@@ -1,15 +1,24 @@
-import { AUTO_HIDE_DURATION, FORM_SUBMISSION_MESSAGE, config } from '@/constants/Config.js';
+import { AUTO_HIDE_DURATION, FORM_SUBMISSION_MESSAGE, NAVIGATE_DURATION, config } from '@/constants/Config.js';
 import axios from 'axios';
-import { useSnackbar } from 'notistack'
+import { useSnackbar } from 'notistack';
+import { useNavigate } from 'react-router-dom';
+
+
 
 const useSubmitFormData = () => {
-    const { enqueueSnackbar } = useSnackbar()
+    const { enqueueSnackbar } = useSnackbar();
+    const navigate = useNavigate();
+
     async function submitFormData(values) {
         console.log(values)
         try {
             const response = await axios.post(`${import.meta.env.VITE_BASE_URL}/api/form/submit`, JSON.stringify(values), config);
             console.log(response);
-            (() => enqueueSnackbar(FORM_SUBMISSION_MESSAGE,  { autoHideDuration: AUTO_HIDE_DURATION, variant: 'success' }))();
+            (() => enqueueSnackbar(FORM_SUBMISSION_MESSAGE, { autoHideDuration: AUTO_HIDE_DURATION, variant: 'success' }))();
+
+            setTimeout(()=>{
+                navigate('/submitted')
+            }, NAVIGATE_DURATION)
 
         } catch (error) {
             console.log(error)

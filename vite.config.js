04371da7cbaaa9d8b17ce0f78d4e4027ec2bea39
manu-hn <manuu.hn11@gmail.com>
2024-06-26 18:04:26 +0530

    "Added build configuration to vite.config.js"

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,9 @@ const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
